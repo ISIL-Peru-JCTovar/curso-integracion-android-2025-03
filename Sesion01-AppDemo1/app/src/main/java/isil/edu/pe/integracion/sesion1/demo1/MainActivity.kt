171de3e855263f39619d1eb4a590eae6e7b26b92
JCTOVARU <jctovarueda@gmail.com>
2025-03-06 22:11:15 -0500

    Completar el desarrollo de la Sesion N° 1: Integracion con Firebase Analytics (Google)

--- Sesion01-AppDemo1/app/src/main/java/isil/edu/pe/integracion/sesion1/demo1/MainActivity.kt
+++ Sesion01-AppDemo1/app/src/main/java/isil/edu/pe/integracion/sesion1/demo1/MainActivity.kt
@@ -1,47 +1,38 @@
 package isil.edu.pe.integracion.sesion1.demo1
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import isil.edu.pe.integracion.sesion1.demo1.ui.theme.ISILSesion01AppDemo1Theme
+import android.widget.*
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.analytics.FirebaseAnalytics
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Invocando el metodo padre
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ISILSesion01AppDemo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        // Seteando el layout main
+        setContentView(R.layout.activity_main)
+        // Inicializar Firebase Analytics
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        // Definiendo un listener para el boton
+        val editText = findViewById<EditText>(R.id.edtNombres)
+        val button = findViewById<Button>(R.id.btnIr)
+        button.setOnClickListener {
+            //-- Enviando el texto hacia un Bundle (que se compartira para la Analitica)
+            val inputText = editText.text.toString()
+            val bundle = Bundle().apply {
+                putString("user_input", inputText)
             }
+            //-- Enviar el evento a Firebase Analytics
+            firebaseAnalytics.logEvent("button_clicked", bundle)
+            //-- Ir a la segunda pantalla
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("user_text", inputText)
+            startActivity(intent)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ISILSesion01AppDemo1Theme {
-        Greeting("Android")
-    }
 }
--- Sesion01-AppDemo1/app/src/main/java/isil/edu/pe/integracion/sesion1/demo1/SecondActivity.kt
+++ Sesion01-AppDemo1/app/src/main/java/isil/edu/pe/integracion/sesion1/demo1/SecondActivity.kt
@@ -1,47 +1,21 @@
 package isil.edu.pe.integracion.sesion1.demo1
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import isil.edu.pe.integracion.sesion1.demo1.ui.theme.ISILSesion01AppDemo1Theme
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class SecondActivity : ComponentActivity() {
+class SecondActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Invocando el metodo padre
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ISILSesion01AppDemo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        // Seteando el layout secundario
+        setContentView(R.layout.activity_second)
+        // Obteniendo los controles
+        val textView = findViewById<TextView>(R.id.txtSaludo)
+        val receivedText = intent.getStringExtra("user_text")
+        // Seteando el mensaje
+        textView.text = getString(R.string.activity_second_input_message, receivedText ?: "")
     }
-}
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    ISILSesion01AppDemo1Theme {
-        Greeting2("Android")
-    }
 }
